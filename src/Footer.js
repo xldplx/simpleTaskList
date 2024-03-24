526934d8dd3af6f948691175c658713f2dfe12bb
Alden Sayidina <aldraddd01@gmail.com>
2024-03-25 05:02:54 +0700

    simpleTaskList v.1.0.0

---
+++ src/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-[100%] bg-purple-700 h-[10vh] flex items-center justify-center">
+            <p className="text-white">&copy; 2024 xldplx</p>
+        </footer>
+    );
+}
---
+++ src/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header className="w-[100%] h-[20vh] bg-gradient-to-br from-purple-400 to-purple-600 flex items-center justify-center">
+            <h1 className="text-[2rem] text-white font-bold shadow-black drop-shadow-xl">
+                Simple Task List
+            </h1>
+        </header>
+    );
+}
