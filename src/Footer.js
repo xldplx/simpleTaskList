f632425d523139e93c6955ddc6bb0e4f2c6cf234
Alden Sayidina <aldraddd01@gmail.com>
2024-03-25 19:07:13 +0700

    html file fixed

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="w-[100%]  h-[10vh] flex shadow-sm border-2 items-center justify-center">
+        <footer className="w-[100%]  h-[10vh] flex border-2 items-center justify-center">
             <p className="font-mono">
                 &copy; 2024 xldplx - simpleTaskList v.1.0.0
             </p>
