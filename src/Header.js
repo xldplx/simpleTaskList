48058b99b9a201fe311d3f15b8262e267114163a
Alden Sayidina <aldraddd01@gmail.com>
2024-03-25 18:55:21 +0700

    redesigned the app

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
-        <header className="w-[100%] h-[20vh] bg-gradient-to-br from-purple-400 to-purple-600 flex items-center justify-center">
-            <h1 className="text-[2rem] text-white font-bold shadow-black drop-shadow-xl">
-                Simple Task List
+        <header className="w-[100%] h-[10vh] border-2 flex items-center justify-center">
+            <h1 className="text-[1.5rem] font-mono font-bold shadow-black underline">
+                simpleTaskList
             </h1>
         </header>
     );
